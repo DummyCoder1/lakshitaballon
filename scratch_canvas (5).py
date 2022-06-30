7be975472c9b1bbeed237ceb6c425b177fc4cbb1
DummyCoder1 <100186733+DummyCoder1@users.noreply.github.com>
2022-06-30 09:55:35 +0530

    Add files via upload

---
+++ scratch_canvas (5).py
@@ -0,0 +1,21 @@
+import turtle, random
+
+def draw_circle():
+    radius = int(input('Enter the radius of circle:'))
+    color = input('Enter the color of circle:')
+    turtle.penup()
+    turtle.fillcolor(color)
+    turtle.goto(random.randint(-150, 150) + 20, random.randint(-150, 150) + 20)
+    (turtle.begin_fill())
+    turtle.circle(radius)
+    (turtle.end_fill())
+    turtle.pendown()
+    turtle.pencolor('black')
+    turtle.goto(0, -200)
+    (turtle.stamp())
+
+''
+draw_circle()
+draw_circle()
+draw_circle()
+draw_circle()
